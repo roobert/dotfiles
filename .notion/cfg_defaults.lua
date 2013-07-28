24858dd39b6a47caf2174760d8f6e4958ec272b2
Rob Wilson <roobert@gmail.com>
2013-07-28 21:38:18 +0100

    updated

--- .notion/cfg_defaults.lua
+++ .notion/cfg_defaults.lua
@@ -1,7 +1,3 @@
---
--- Notion default settings
---
-
 dopath("cfg_notioncore")
 dopath("cfg_kludges")
 dopath("cfg_layouts")
@@ -23,40 +19,16 @@ randr_screen_change_notify_hook = ioncore.get_hook('randr_screen_change_notify')
 if randr_screen_change_notify_hook ~= nil then
     randr_screen_change_notify_hook:add(screenlayoutupdated)
 end
--- $Id$
 
---XTERM="xterm"
---XTERM="mlterm"
-
--- broken! https://bugs.launchpad.net/ubuntu/+source/compiz/+bug/861268
+-- urxvt is broken on my work machine..? https://bugs.launchpad.net/ubuntu/+source/compiz/+bug/861268
 XTERM="urxvt"
 BROWSER="chromium-browser"
 BROWSER_SECRET="chromium-browser --user-data-dir=~/.chromium-noproxy --incognito"
 MUSIC="spotify"
 
-defbindings("WScreen", {
-    kpress(META.."F2", "notioncore.exec_on(_, XTERM or 'x-terminal-emulator')"),
-})
-
+-- for some reason need this as well as the stuff below to disable F12..
 defbindings("WScreen", {
     kpress(ALTMETA.."F12", nil),
-})
-
-defbindings("WScreen", {
-    kpress(META.."F2", "notioncore.exec_on(_, XTERM or 'x-terminal-emulator')"),
-    kpress(META.."F3", "mod_query.query_exec(_)"),
-    kpress(META.."F4", "notioncore.exec_on(_, MUSIC or 'x-music')"),
-    kpress(META.."F5", "notioncore.exec_on(_, BROWSER or 'x-browser')"),
-    kpress(META.."F6", "notioncore.exec_on(_, BROWSER_SECRET or 'x-browser-secret')"),
-    kpress(META.."F9", "ioncore.create_ws(_)"),
-    kpress(META.."F12", "mod_query.query_menu(_, _sub, 'mainmenu', 'Main menu:')"),
-
-    kpress("XF86AudioPlay", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause')"),
-    kpress("XF86AudioNext", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next')"),
-    kpress("XF86AudioPrev", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous')"),
-    kpress("XF86AudioRaiseVolume", "notioncore.exec('amixer set Master 5%+')"),
-    kpress("XF86AudioLowerVolume", "notioncore.exec('amixer set Master 5%-')"),
-    kpress("XF86AudioMute", "notioncore.exec('amixer -D pulse set Master 1+ toggle')"),
 })
 
 defbindings("WMPlex.toplevel", {
@@ -85,44 +57,41 @@ defbindings("WMPlex.toplevel", {
     kpress(META.."F12", nil),
 })
 
-if not gr.select_engine("de") then
-	return
-end
+defbindings("WScreen", {
+    kpress(META.."F2", "notioncore.exec_on(_, XTERM or 'x-terminal-emulator')"),
+    kpress(META.."F3", "mod_query.query_exec(_)"),
+    kpress(META.."F4", "notioncore.exec_on(_, MUSIC or 'x-music')"),
+    kpress(META.."F5", "notioncore.exec_on(_, BROWSER or 'x-browser')"),
+    kpress(META.."F6", "notioncore.exec_on(_, BROWSER_SECRET or 'x-browser-secret')"),
+    kpress(META.."F9", "ioncore.create_ws(_)"),
+    kpress(META.."F12", "mod_query.query_menu(_, _sub, 'mainmenu', 'Main menu:')"),
+
+    kpress("XF86AudioPlay", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause')"),
+    kpress("XF86AudioNext", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next')"),
+    kpress("XF86AudioPrev", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous')"),
+    kpress("XF86AudioRaiseVolume", "notioncore.exec('amixer set Master 5%+')"),
+    kpress("XF86AudioLowerVolume", "notioncore.exec('amixer set Master 5%-')"),
+    kpress("XF86AudioMute", "notioncore.exec('amixer -D pulse set Master 1+ toggle')"),
+})
+
+if not gr.select_engine("de") then return end
 
 de.reset()
-
--- font = "xft: Sans-8"
-
--- mainfont = "xft: Sans-8"
--- boldfont = "xft: Sans-8:weight=bold"
--- bigfont = "xft: Sans-14"
--- bigboldfont = "xft: Sans-14:weight=bold"
 
 de.defstyle("*", {
 	background_colour = "#000",
 	foreground_colour = "#777",
-
-	shadow_pixels = 0,
-	highlight_pixels = 0,
-	padding_pixels = 0,
-	spacing = 0,
-
---	font = "-*-helvetica-medium-r-normal-*-12-*-*-*-*-*-*-*",
--- font = "fixed",
---font = "Ubuntu Mono",
-
-font = "-misc-ubuntu-medium-r-normal--12-0-0-0-p-0-iso10646-1",
-
---	font = "xft: Ubuntu-14",
-	font = "xft: ubuntu mono-12",
-
-	text_align = "center",
+	shadow_pixels     = 0,
+	highlight_pixels  = 0,
+	padding_pixels    = 0,
+	spacing           = 0,
+  font              = "-misc-ubuntu-medium-r-normal--12-0-0-0-p-0-iso10646-1",
+	font              = "xft: ubuntu mono-12",
+	text_align        = "center",
 })
 
 de.defstyle("tab", {
-	de.substyle("inactive-selected", {
---		foreground_colour = "#666",
-	}),
+	de.substyle("inactive-selected", {}),
 
 	de.substyle("active-selected", {
 		foreground_colour = "#fff",
@@ -132,8 +101,8 @@ de.defstyle("tab", {
 de.defstyle("frame", {
 	transparent_background = false,
 
-	padding_pixels = 2,
-	shadow_pixels = 1,
+	padding_pixels   = 2,
+	shadow_pixels    = 1,
 	highlight_pixels = 1,
 
 	de.substyle("inactive", {
@@ -165,9 +134,6 @@ de.defstyle("stdisp", {
 -- Define some additional title shortening rules to use when the full
 -- title doesn't fit in the available space. The first-defined matching
 -- rule that succeeds in making the title short enough is used.
-ioncore.defshortening("(.*) - Mozilla(<[0-9]+>)", "$1$2$|$1$<...$2")
-ioncore.defshortening("(.*) - Mozilla", "$1$|$1$<...")
-ioncore.defshortening("XMMS - (.*)", "$1$|...$>$1")
 ioncore.defshortening("[^:]+: (.*)(<[0-9]+>)", "$1$2$|$1$<...$2")
 ioncore.defshortening("[^:]+: (.*)", "$1$|$1$<...")
 ioncore.defshortening("(.*)(<[0-9]+>)", "$1$2$|$1$<...$2")
@@ -175,4 +141,3 @@ ioncore.defshortening("(.*)", "$1$|$1$<...")
 
 -- Refresh objects' brushes.
 gr.refresh()
-
