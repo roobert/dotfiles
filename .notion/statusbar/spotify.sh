3b892fbca5ad53a8eaaa006e9bfe383a8fe12d1f
Rob Wilson <roobert@gmail.com>
2013-08-17 20:50:49 +0100

    updated

--- .notion/statusbar/spotify.sh
+++ .notion/statusbar/spotify.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-METADATA="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata')"
+METADATA="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2> /dev/null)"
+
+echo $METADATA | grep artist > /dev/null
+
+if [[ $? != 0 ]]; then
+  exit
+fi
 
 TITLE="$(echo $METADATA  | sed 's/.*"xesam:title" variant string "\([^"]*\).*/\1/')"
 ARTIST="$(echo $METADATA | sed 's/.*"xesam:artist" variant array \[ string "\([^"]*\).*/\1/')"
