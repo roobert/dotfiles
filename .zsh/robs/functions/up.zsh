8237a80f84f9b8daec41de6492dcc839778da5bb
Rob Wilson <roobert@gmail.com>
2013-09-02 12:18:59 +0100

    updated

--- .zsh/robs/functions/up.zsh
+++ .zsh/robs/functions/up.zsh
@@ -1,12 +1,14 @@
 # thanks mattf for implimenting stuff so i dont have to :)
 
+# this lovely stuff means you can cd to directories that are further up the directory tree for $CWD
+
 function up {
 
   dir=$PWD;
 
   while [ $dir != "/" ]; do
     if [ $dir:t = $1 ]; then
-      cd $dir
+      builtin cd $dir
       return 0
     fi
 
@@ -17,5 +19,7 @@ function up {
 }
 
 function cd {
-  builtin cd $* &>/dev/null || up $*
+    if ! builtin cd $* &>/dev/null; then
+        up $* || builtin cd $*
+    fi
 }
