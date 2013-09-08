ea10445532d1d50a101526c4fbd36c409aecf3b3
Rob Wilson <roobert@gmail.com>
2013-09-08 08:40:55 +0100

    updated

---
+++ .zsh/robs/functions/make_gallery.zsh
@@ -0,0 +1,12 @@
+
+
+function make_gallery {
+
+  if [ -f 'index.html' ]; then rm index.html; fi
+
+  for i in *; do
+    echo "<a href="$i"><img src='$i' /></a>" >> index.html
+  done
+
+  echo "<style>img { height: 300px; margin: 3px; }</style>" >> index.html
+}
