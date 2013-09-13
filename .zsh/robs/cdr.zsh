043d066c8516fdb16ade659fcc6d02be8f8c94f9
Rob Wilson <roobert@gmail.com>
2013-09-13 13:58:48 +0100

    updated

--- .zsh/robs/cdr.zsh
+++ .zsh/robs/cdr.zsh
@@ -1,3 +1,8 @@
 # super awesome cdr! - http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Recent-Directories
-autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
-add-zsh-hook chpwd chpwd_recent_dirs
+# list files as well as dirs when cd tab completing                                                            
+autoload -U is-at-least
+
+if is-at-least 4.4.0; then
+  autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+  add-zsh-hook chpwd chpwd_recent_dirs
+fi
