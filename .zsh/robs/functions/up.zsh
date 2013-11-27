8c96be9de0ae312ede2d01c91860aa534a00141a
Rob Wilson <roobert@gmail.com>
2013-11-27 18:36:30 +0000

    updated

--- .zsh/robs/functions/up.zsh
+++ .zsh/robs/functions/up.zsh
@@ -4,8 +4,6 @@
 
 function up {
 
-  echo "up!!!"
-
   if [[ $# == 0 ]]; then
     if [[ -z "${UP_PATHS}" ]]; then
       echo "no up paths, boo"
@@ -15,25 +13,25 @@ function up {
     IFS=";"
 
     for up_path in $UP_PATHS; do
-      echo "up_path: $up_path"
+      echo "up_path: ${up_path}"
       up $(pwd | sed -e "s/.*${up_path}\/\([^/]*\)\/.*/\1/")
       if [[ $? == 0 ]]; then
         return 0
       fi
     done
   else
-    target=$1
+    target="$1"
   fi
 
   dir=$PWD
 
   while [ $dir != "/" ]; do
-    if [ $dir:t = $target ]; then
-      builtin cd $dir
+    if [ "$dir:t" = "$target" ]; then
+      builtin cd "$dir"
       return 0
     fi
 
-    dir=$dir:h
+    dir="$dir:h"
   done
 
   return 1
