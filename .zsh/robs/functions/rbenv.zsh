534470fd29c4ac5ca6d75a8221946be270b858f8
Rob Wilson <roobert@gmail.com>
2013-12-04 20:38:08 +0000

    updated

---
+++ .zsh/robs/functions/rbenv.zsh
@@ -0,0 +1,8 @@
+function rbenv.install {
+  if type -f git 2>&1 >/dev/null; then
+    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+  else
+    echo "git is not installed!"
+  fi
+}
