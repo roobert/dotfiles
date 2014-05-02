43a8f0019032a2533866fc7384dee9c2a213c79d
Rob Wilson <roobert@gmail.com>
2014-05-02 11:02:32 +0100

    updated

--- .zsh/robs/functions/logs.zsh
+++ .zsh/robs/functions/logs.zsh
@@ -1,11 +1,32 @@
 #!/usr/bin/env zsh
 
-# usage: $0 /var/log/sams_server/access_log*~*.gz
+# useful for checking access logs for requests per minute.. yay awk!
+
+# usage: $0 access_log*~*.gz
+
 function requests_per_minute () {
   for log in $*; do
-    echo -n "$log: "
+    echo -n "$log: average: "
 
     cat $log | awk '{ print $4  }' | cut -d':' -f 2-3 | uniq -c | \
+      awk 'BEGIN { count = 0; total = 0  } { count += 1; total += $1  } END { print total/count  }'
+  done
+}
+
+function requests_per_minute_95th_percentile () {
+  for log in $*; do
+
+    REQUESTS_PER_MINUTE_DATA="$(cat $log| awk '{ print $4  }' | cut -d':' -f 2-3 | uniq -c | sort -n)"
+
+    NUMBER_OF_REQUESTS_PER_MINUTE="$(echo ${REQUESTS_PER_MINUTE_DATA} | wc -l | awk '{ print $1  }')"
+
+    NUMBER_OF_REQUESTS_IN_95TH_PERCENTILE="$(($(($NUMBER_OF_REQUESTS_PER_MINUTE * 95)) / 100))"
+
+    PEAK="$(echo $REQUESTS_PER_MINUTE_DATA| tail -n 1)"
+
+    echo -n "$log: (peak: $PEAK) - 95th percentile average: "
+
+    echo $REQUESTS_PER_MINUTE_DATA | head -n $NUMBER_OF_REQUESTS_IN_95TH_PERCENTILE | \
       awk 'BEGIN { count = 0; total = 0  } { count += 1; total += $1  } END { print total/count  }'
   done
 }
