782e097b2d211501f3f402d144fec37412b5a9e7
Rob Wilson <roobert@gmail.com>
2014-12-04 22:46:56 +0000

    updated

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output eDP1 --mode 1920x1200 --pos 0x0 --rotate normal --output VGA1 --off
