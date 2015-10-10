4168db7ec2f882623c61a36f980ebcbf454abc9a
Rob Wilson <roobert@gmail.com>
2015-10-10 22:36:14 +0100

    updated

--- .zsh/robs/fzf.zsh
+++ .zsh/robs/fzf.zsh
@@ -1,3 +1,3 @@
 export PATH="$PATH:$HOME/.fzf/bin"
 export FZF_COMPLETION_TRIGGER='@'
-export FZF_DEFAULT_OPTS='--extended --color=fg:248,bg:232,hl:118,fg+:232,bg+:238,hl+:106 --color=info:060,prompt:156,spinner:208,pointer:156,marker:208'
+export FZF_DEFAULT_OPTS='--extended-exact +s --color=fg:248,bg:232,hl:118,fg+:232,bg+:238,hl+:106 --color=info:060,prompt:156,spinner:208,pointer:156,marker:208'
