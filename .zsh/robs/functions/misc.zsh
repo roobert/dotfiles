df17a4aaa4ecafd522117c6ebf59f2f095191be6
Rob Wilson <roobert@gmail.com>
2015-10-26 19:21:20 +0000

    updated

--- .zsh/robs/functions/misc.zsh
+++ .zsh/robs/functions/misc.zsh
@@ -14,32 +14,6 @@ function get_stuff_from_oh_my_zsh () {
 # shortcuts to install some useful tools..
 #
 
-# list useful stuff like aliases and functions along with description
-function help {
-cat <<- EOF
-	# cheat sheet
-	^e            - prefix line with 'vim', e.g: ^e^g would create 'vim \$()'
-	^g            - wrap cli input in '\$()' (group command)
-	^p            - open vim +:CtrlP
-	vp <dir>      - open vim +:CtrlP in specified dir
-	hl            - highlight <string>
-	g             - grep (ignore .git)
-	f             - find . -name '<string>'   (find)
-	fw            - find . -name '*<string>*' (find wild)
-	mcd           - make and cd into (nested) dir
-	dus           - du sorted by size
-	mhi           - make html index for images
-	
-	# FZF
-	^f            - vim fzf
-	vf            - vim <path> (fzf)
-	<cmd> @<tab>  - fzf generic
-	^r            - reverse-i-search fzf
-	fgrep         - fzf grep
-	kill <tab>    - fzf kill
-EOF
-}
-
 # install: subversion vim zsh tree colordiff ncdu htop ack-grep apt-file notion rxvt-unicode-256color
 function install_common_tools {
   sudo apt-get install git subversion vim zsh tree colordiff ncdu htop ack-grep apt-file notion rxvt-unicode-256color autocutsel tcpdump
