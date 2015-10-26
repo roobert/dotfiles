5403bf682c412ec55043ea329a1e89317147d53a
Rob Wilson <roobert@gmail.com>
2015-10-26 23:37:06 +0000

    updated

--- .zsh/robs/fzf.zsh
+++ .zsh/robs/fzf.zsh
@@ -1,4 +1,9 @@
 export PATH="$PATH:$HOME/.fzf/bin"
 export FZF_COMPLETION_TRIGGER='@'
-export FZF_DEFAULT_OPTS='--extended-exact +s --color=fg:248,bg:232,hl:118,fg+:232,bg+:238,hl+:106 --color=info:060,prompt:156,spinner:208,pointer:156,marker:208'
 export FZF_TMUX_HEIGHT="25%"
+export FZF_DEFAULT_OPTS='
+  --extended-exact
+  +s
+  --color=fg:-1,bg:-1,hl:118,fg+:-1,bg+:-1,hl+:200
+  --color=info:060,prompt:118,spinner:208,pointer:156,marker:208
+'
