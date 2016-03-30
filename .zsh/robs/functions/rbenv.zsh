e4073b7d72e5a088ed2a1b27eb0726e97e65513a
Rob Wilson <roobert@gmail.com>
2016-03-30 18:13:00 +0100

    updated

--- .zsh/robs/functions/rbenv.zsh
+++ .zsh/robs/functions/rbenv.zsh
@@ -1,7 +1,7 @@
 function install_rbenv {
   if type -f git 2>&1 >/dev/null; then
-    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+    git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+    git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
   else
     echo "git is not installed!"
   fi
