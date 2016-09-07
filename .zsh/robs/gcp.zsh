49b48d06b32ebcb5de9fbdb5431d81e362c7c909
Rob Wilson <roobert@gmail.com>
2016-09-07 10:53:45 +0100

    updated

---
+++ .zsh/robs/gcp.zsh
@@ -0,0 +1,2 @@
+test -f ~/opt/google-cloud-sdk/completion.zsh.inc && source ~/opt/google-cloud-sdk/completion.zsh.inc
+test -f ~/opt/google-cloud-sdk/path.zsh.inc && source ~/opt/google-cloud-sdk/path.zsh.inc
