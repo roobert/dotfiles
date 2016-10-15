c350d33127469db83299185f575bb0ea4d9f58ad
Rob Wilson <roobert@gmail.com>
2016-10-15 15:03:00 +0100

    updated

--- .tmuxifier/layouts/network.window.sh
+++ .tmuxifier/layouts/network.window.sh
@@ -19,8 +19,9 @@ select_pane 3
 run_cmd "sudo iftop -i wlx00c0ca8fe5b8 -b"
 send_keys "t"
 split_v 50
-run_cmd "sudo bmon -p 'wlx*'"
-send_keys "di"
+#run_cmd "sudo bmon -p 'wlx*'"
+#send_keys "di"
+run_cmd "sudo nload wlx00c0ca8fe5b8"
 
 # bottom right
 split_v 50
