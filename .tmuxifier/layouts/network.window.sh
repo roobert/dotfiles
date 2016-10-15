cabc2ef47995eb9a8a2fae32ce1d2a47890b5569
Rob Wilson <roobert@gmail.com>
2016-10-15 14:39:06 +0100

    updated

--- .tmuxifier/layouts/network.window.sh
+++ .tmuxifier/layouts/network.window.sh
@@ -1,21 +1,26 @@
 new_window "network"
 
-# pane 0
+# top left (0)
 run_cmd "sudo nethogs"
 
-# pane 1
+# bottom left (2)1
+split_h 50
+select_pane 0
+
 split_v 50
 run_cmd "sudo wavemon"
 
+split_v 50
+run_cmd "sudo wavemon"
+send_keys "<F2>"
+
 # pane 2
-select_pane 0
-split_h 50
+select_pane 3
 run_cmd "sudo iftop -i wlx00c0ca8fe5b8 -b"
 send_keys "t"
 
 # pane 3
-select_pane 2
-split_h 50
+split_v 50
 run_cmd "watch -n1 'nmcli d wifi'"
 
 split_h 50
