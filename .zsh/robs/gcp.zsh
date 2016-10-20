0ca4bcfeb1386aa54b2eb0078b091a8d18566c21
Rob Wilson <roobert@gmail.com>
2016-10-20 11:04:56 +0100

    updated

--- .zsh/robs/gcp.zsh
+++ .zsh/robs/gcp.zsh
@@ -1,2 +1,2 @@
-test -f ~/opt/google-cloud-sdk/completion.zsh.inc && source ~/opt/google-cloud-sdk/completion.zsh.inc
-test -f ~/opt/google-cloud-sdk/path.zsh.inc && source ~/opt/google-cloud-sdk/path.zsh.inc
+test -f $HOME/opt/google-cloud-sdk/completion.zsh.inc && source $HOME/opt/google-cloud-sdk/completion.zsh.inc
+test -f $HOME/opt/google-cloud-sdk/path.zsh.inc && source $HOME/opt/google-cloud-sdk/path.zsh.inc
