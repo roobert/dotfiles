c0caf3bbdf5ecf1cf1966967b4c766a70470a121
Rob Wilson <roobert@gmail.com>
2016-10-20 11:14:43 +0100

    updated

--- .screenlayout/work-laptop.sh
+++ .screenlayout/work-laptop.sh
@@ -6,6 +6,6 @@
 #xrandr --output DP1 --off
 #xrandr --output HDMI3 --off
 
-xrandr --output eDP1  --mode 1600x1024 --pos 0x0 --rotate normal --below HDMI2
-xrandr --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --above eDP1
-xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --left-of HDMI2
+xrandr --output eDP-1  --mode 1600x1024 --pos 0x0 --rotate normal --below HDMI-2
+xrandr --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal --above eDP-1
+xrandr --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --left-of HDMI-2
