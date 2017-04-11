1e72bb3c9f402734f56d6c089a644e2e96446103
Rob Wilson <roobert@gmail.com>
2017-04-11 19:57:18 +0100

    updated

--- .zsh/robs/prompt.zsh
+++ .zsh/robs/prompt.zsh
@@ -4,19 +4,23 @@
 
 # colour chart:  http://upload.wikimedia.org/wikipedia/commons/9/95/Xterm_color_chart.png or use 'spectrum_ls'
 
-source $HOME/.zsh/zsh-git-prompt/zshrc.sh
+autoload -U colors
+colors
+setopt PROMPT_SUBST
 
-ZSH_THEME_GIT_PROMPT_PREFIX=""
-ZSH_THEME_GIT_PROMPT_SUFFIX=""
-ZSH_THEME_GIT_PROMPT_SEPARATOR=""
-ZSH_THEME_GIT_PROMPT_BRANCH=" %{$FG[060]%}"
-ZSH_THEME_GIT_PROMPT_STAGED=" %{$fg[yellow]%}%{\u26ab%G%}"
-ZSH_THEME_GIT_PROMPT_CONFLICTS=" %{$fg[red]%}%{\u2716%G%}"
-ZSH_THEME_GIT_PROMPT_CHANGED=" %{$fg[blue]%}%{\u271a%G%}"
-ZSH_THEME_GIT_PROMPT_BEHIND=" %{\u2193%G%}"
-ZSH_THEME_GIT_PROMPT_AHEAD=" %{$FG[166]%}%{\u2191%G%}"
-ZSH_THEME_GIT_PROMPT_UNTRACKED=" %{$fg[red]%}%{\u2717%G%}"
-ZSH_THEME_GIT_PROMPT_CLEAN=" %{$FG[028]%}%{\u2713%G%}"
+#source $HOME/.zsh/zsh-git-prompt/zshrc.sh
+#
+#ZSH_THEME_GIT_PROMPT_PREFIX=""
+#ZSH_THEME_GIT_PROMPT_SUFFIX=""
+#ZSH_THEME_GIT_PROMPT_SEPARATOR=""
+#ZSH_THEME_GIT_PROMPT_BRANCH=" %{$FG[060]%}"
+#ZSH_THEME_GIT_PROMPT_STAGED=" %{$fg[yellow]%}%{\u26ab%G%}"
+#ZSH_THEME_GIT_PROMPT_CONFLICTS=" %{$fg[red]%}%{\u2716%G%}"
+#ZSH_THEME_GIT_PROMPT_CHANGED=" %{$fg[blue]%}%{\u271a%G%}"
+#ZSH_THEME_GIT_PROMPT_BEHIND=" %{\u2193%G%}"
+#ZSH_THEME_GIT_PROMPT_AHEAD=" %{$FG[166]%}%{\u2191%G%}"
+#ZSH_THEME_GIT_PROMPT_UNTRACKED=" %{$fg[red]%}%{\u2717%G%}"
+#ZSH_THEME_GIT_PROMPT_CLEAN=" %{$FG[028]%}%{\u2713%G%}"
 
 #HOST=$(hostname -f | tr '.' '\n' | tac | tr '\n' '.' | sed 's/\.$/\n/')
 #PHOST="%{$FG[240]%}${HOST}"
@@ -37,12 +41,13 @@ RPS1=''
 #    (*)          VI_MODE="$MODE_INS" ;;
 #  esac
 #
-#  #if kubectl config current-context >/dev/null 2>&1; then
-#  #  K8_CONTEXT=$(kubectl config current-context)
-#  #  PROJECT=$(echo $K8_CONTEXT | cut -d_ -f2)
-#  #  CLUSTER=$(echo $K8_CONTEXT | cut -d_ -f4)
-#  #  KUBERNETES_CONTEXT="%{$FG[111]%}k8s%{$FX[reset]%}:%{$FG[097]%}${PROJECT}/${CLUSTER}%{$FX[reset]%} "
-#  #fi
+#  if kubectl config current-context >/dev/null 2>&1; then
+#    K8_CONTEXT=$(kubectl config current-context)
+#    PROJECT=$(echo $K8_CONTEXT | cut -d_ -f2)
+#    CLUSTER=$(echo $K8_CONTEXT | cut -d_ -f4)
+#    KUBERNETES_CONTEXT="%{$FG[111]%}k8s%{$FX[reset]%}:%{$FG[097]%}${PROJECT}/${CLUSTER}%{$FX[reset]%} "
+#  fi
+#
 #  KUBERNETES_CONTEXT=""
 #
 #  unset GIT_STASHES
@@ -58,8 +63,7 @@ RPS1=''
 #  zle reset-prompt
 #}
 
-
-PS1='$PHOST $PWHERE$(git_super_status) ${GIT_STASHES}$KUBERNETES_CONTEXT$EXIT_STATUS${VI_MODE}>%{$FX[reset]%} '
+PS1='$PHOST $PWHERE >%{$FX[reset]%} '
 
 zle -N zle-line-init
 zle -N zle-keymap-select
