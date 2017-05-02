413bb69db1148580ffecfde6fc7c80e3e0e04b17
Rob Wilson <roobert@gmail.com>
2017-05-02 22:15:38 +0100

    updated

--- .tmux/tmux-git.sh
+++ .tmux/tmux-git.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+NO_REMOTE_TRACKING_SYMBOL="L"
+BRANCH_SYMBOL="⎇ "
+STAGED_SYMBOL="●"
+CONFLICT_SYMBOL="✖"
+CHANGED_SYMBOL="✚"
+UNTRACKED_SYMBOL="…"
+STASHED_SYMBOL="⚑"
+CLEAN_SYMBOL="✔"
+AHEAD_SYMBOL="↑·"
+BEHIND_SYMBOL="↓·"
+PREHASH_SYMBOL=":"
+
+#$(~/.tmux-gitbar/scripts/gitstatus.sh .)
+
 current_path="$(tmux display-message -p -F "#{pane_current_path}")"
 
 repo=$(basename "$(git -C $current_path rev-parse --show-toplevel 2> /dev/null)")
