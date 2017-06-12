f2d8ae26c5790c2d1c5bbe7fbac649247cf426d9
Rob Wilson <roobert@gmail.com>
2017-06-12 10:56:10 +0100

    updated

---
+++ .zsh/robs/bin/k8s-current-context.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+current_context_file=~/.k8s-current-context
+current_context=$(kubectl config current-context)
+
+project=$(echo ${current_context} | cut -d_ -f2)
+zone=$(echo ${current_context} | cut -d_ -f3)
+cluster=$(echo ${current_context} | cut -d_ -f4)
+
+cat << EOF > ${current_context_file}.new
+K8S_CURRENT_CONTEXT_PROJECT=${project}
+K8S_CURRENT_CONTEXT_ZONE=${zone}
+K8S_CURRENT_CONTEXT_CLUSTER=${cluster}
+EOF
+
+cat ${current_context_file}
+
+mv -v ${current_context_file}.new ${current_context_file}
