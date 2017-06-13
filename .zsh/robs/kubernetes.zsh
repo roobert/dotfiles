cc45c02e47c808a5093d5bc8d2b597adca23dcd2
Rob Wilson <roobert@gmail.com>
2017-06-13 10:26:20 +0100

    updated

--- .zsh/robs/kubernetes.zsh
+++ .zsh/robs/kubernetes.zsh
@@ -1,6 +1,6 @@
 function podname () {
-  pod=$1
-  namespace=${2:-default}
+  namespace=${1}
+  pod=$2
 
   pod_id=$(kubectl get pods -n $namespace -o name | cut -d\/ -f2- | grep $pod)
 
@@ -14,18 +14,18 @@ function podname () {
 #$(kubectl get pods --namespace monitoring -l app=influxdb-influxdb -o jsonpath='{ .items[0].metadata.name  }')
 
 function klog () {
-  pod=$1
-  namespace=${2:-default}
+  namespace=${1:-default}
+  pod=$2
 
-  kubectl logs -f --namespace $namespace $(podname $pod $namespace)
+  kubectl logs -f --namespace $namespace $(podname $namespace $pod)
 }
 
 function kexec () {
-  pod=$1
-  command=${2:-/bin/bash}
-  namespace=${3:-default}
+  namespace=${1}
+  pod=$2
+  command=${3:-/bin/bash}
 
-  kubectl exec --namespace $namespace $(podname $pod $namespace) -ti -- $command
+  kubectl exec --namespace $namespace $(podname $namespace $pod) -ti -- $command
 }
 
 function kcluster () {
