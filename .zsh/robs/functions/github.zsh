835fa710590edb936e4657008fe6fc8d107ecd8b
Rob Wilson <roobert@gmail.com>
2017-09-10 17:00:43 +0100

    updated

--- .zsh/robs/functions/github.zsh
+++ .zsh/robs/functions/github.zsh
@@ -1,8 +1,8 @@
 #
 # # initial setup
 # git init --bare $HOME/.dotfiles
-# git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote add origin git@github.com:roobert/dotfiles
 # git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+# git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote add origin git@github.com:roobert/dotfiles
 #
 # # on a new machine
 # git clone git@github.com:roobert/dotfiles .dotfiles --bare
