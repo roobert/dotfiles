5fe24df4334e5987aaa4128a15351b8aff4dd607
Rob Wilson <roobert@gmail.com>
2017-09-20 19:20:39 +0100

    check for kubectl

--- .zsh/robs/kubectl.zsh
+++ .zsh/robs/kubectl.zsh
@@ -1,1 +1,1 @@
-source <(kubectl completion zsh | sed -e '/flaghash.*true/s/# pad/2>\/dev\/null # pad/')
+which kubectl 2> /dev/null 2>&1 && source <(kubectl completion zsh | sed -e '/flaghash.*true/s/# pad/2>\/dev\/null # pad/')
