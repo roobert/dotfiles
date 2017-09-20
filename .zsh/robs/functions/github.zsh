974861c118382972d83a48b4db90c21ecb046980
Rob Wilson <roobert@gmail.com>
2017-09-20 18:49:36 +0100

    update dotfile docs

--- .zsh/robs/functions/github.zsh
+++ .zsh/robs/functions/github.zsh
@@ -7,6 +7,7 @@
 # # on a new machine
 # git clone git@github.com:roobert/dotfiles .dotfiles --bare
 # git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+# git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
 
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias dfs="dotfiles"
