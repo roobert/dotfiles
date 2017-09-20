75ff3141f6204850a91b17987d012a1cc5fcdf83
Rob Wilson <roobert@gmail.com>
2017-09-20 19:21:43 +0100

    fix check for kubectl

--- .zsh/robs/kubectl.zsh
+++ .zsh/robs/kubectl.zsh
@@ -1,1 +1,1 @@
-which kubectl 2> /dev/null 2>&1 && source <(kubectl completion zsh | sed -e '/flaghash.*true/s/# pad/2>\/dev\/null # pad/')
+which kubectl > /dev/null 2>&1 && source <(kubectl completion zsh | sed -e '/flaghash.*true/s/# pad/2>\/dev\/null # pad/')
