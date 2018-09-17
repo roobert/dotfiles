a91063044d0f1de493682204abca980651349836
Rob Wilson <roobert@gmail.com>
2018-09-17 11:35:38 +0100

    add node path

--- .zsh/robs/path_stuff.zsh
+++ .zsh/robs/path_stuff.zsh
@@ -4,7 +4,7 @@ if [ -d "$HOME/work/systems/pm" ]; then
 fi
 
 # include my paths in path
-MY_PATHS=($HOME/bin $HOME/opt/vim/bin $HOME/.local/bin/ $HOME/.tmuxifier/bin $HOME/.zsh/git-stashes $HOME/opt/blackbox)
+MY_PATHS=($HOME/bin $HOME/opt/vim/bin $HOME/.local/bin/ $HOME/.tmuxifier/bin $HOME/.zsh/git-stashes $HOME/opt/blackbox $HOME/opt/node/bin)
 
 for my_path in $MY_PATHS; do
   if [ -d "$my_path" ]; then
