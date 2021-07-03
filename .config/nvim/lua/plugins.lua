994f4b796d41934350e4e71af38a2114e3fe7446
Rob Wilson <roobert@gmail.com>
2021-07-03 14:51:53 +0100

    updated

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,12 @@
+-- auto install Packer if it doesn't exist..
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+    api.nvim_command 'packadd packer.nvim'
+    require('plugins')
+    cmd 'PackerInstall'
+end
+
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
---
+++ .config/nvim/lua/python.lua
@@ -0,0 +1,2 @@
+-- Specify location of python for use with NeoVim
+vim.g['python3_host_prog'] = os.getenv('HOME') .. '/.pyenv/versions/3.9.5/bin/python'
