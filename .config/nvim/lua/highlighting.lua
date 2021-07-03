ad98b2bf06a5fc0aee8543cc2c0e31f21fd253bc
Rob Wilson <roobert@gmail.com>
2021-07-03 14:35:25 +0100

    updated

--- .config/nvim/lua/highlighting.lua
+++ .config/nvim/lua/highlighting.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = 'maintained',
-  highlight = {enable = true},
-  indent = {enable = true},
+    ensure_installed = 'maintained',
+    highlight = {enable = true},
+    indent = {enable = true}
 }
---
+++ .config/nvim/lua/spelling.lua
@@ -0,0 +1,3 @@
+vim.cmd [[autocmd FileType text,latex,tex,md,markdown setlocal spell]]
+vim.cmd [[autocmd FileType text,latex,tex,md,markdown set spell]]
+vim.cmd [[autocmd FileType text,latex,tex,md,markdown lua vim.opt.spell=true]]
