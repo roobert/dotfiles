821836d236b66fa5a73eed252a1507aa5d519677
Rob Wilson <roobert@gmail.com>
2021-07-13 20:35:58 +0100

    Updated

--- .config/lv-user/options.lua
+++ .config/lv-user/options.lua
@@ -1,9 +1,15 @@
+-- FIXME: these are ignored
+
 vim.opt.listchars = {
-    tab = '→ ',
-    eol = '↲',
-    nbsp = '␣',
-    trail = '•',
-    extends = '⟩',
-    precedes = '⟨',
-    space = '␣'
+  tab = "→ ",
+  eol = "↲",
+  nbsp = "␣",
+  trail = "•",
+  extends = "⟩",
+  precedes = "⟨",
+  space = "␣",
 }
+
+-- Undo setting from lunarvim - wrapping cursor movement across lines
+vim.cmd "set whichwrap=b,s"
+vim.cmd "set iskeyword+=_"
