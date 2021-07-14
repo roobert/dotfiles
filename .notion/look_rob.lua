9321075c031c26ea149b604333ebb476179056e5
Rob Wilson <roobert@gmail.com>
2021-07-14 14:22:56 -0700

    updated

--- .notion/look_rob.lua
+++ .notion/look_rob.lua
@@ -1,47 +1,99 @@
-XTERM="$HOME/bin/kitty"
-BROWSER="firefox"
-SCREENLOCK="bash -c 'sleep 1; xset dpms force off'; dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock"
+--META="Mod1+"
+--MOD1="Mod1+"
 
-defbindings("WScreen", {
-    kpress(META.."F12", nil),
-})
+--XTERM="kitty"
+--BROWSER="google-chrome"
 
-defbindings("WMPlex.toplevel", {
-    kpress(META.."F1", nil),
-    kpress(META.."F2", nil),
-    kpress(META.."F3", nil),
-    kpress(META.."F4", nil),
-    kpress(META.."F5", nil),
-    kpress(META.."F6", nil),
-    kpress(META.."F7", nil),
-    kpress(META.."F8", nil),
-    kpress(META.."F9", nil),
-    kpress(META.."F10", nil),
-    kpress(META.."F11", nil),
-    kpress(META.."F12", nil),
-    kpress(META.."L", nil),
-})
+--SCREENLOCK="bash -c 'sleep 1; xset dpms force off'; dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock"
 
-defbindings("WScreen", {
-    kpress(META.."F2", "notioncore.exec_on(_, XTERM or 'x-terminal-emulator')"),
-    kpress(META.."F3", "mod_query.query_exec(_)"),
-    kpress(META.."F4", "notioncore.exec_on(_, BROWSER or 'x-browser')"),
-    kpress(META.."F5", "notioncore.exec_on(_, BROWSER_SECRET or 'x-browser-secret')"),
-    kpress(META.."F6", "notioncore.exec_on(_, MUSIC or 'x-music')"),
-    kpress(META.."F10", "ioncore.create_ws(_)"),
-    kpress(META.."F11", "ioncore.restart()"),
-    kpress(META.."F12", "mod_query.query_menu(_, _sub, 'mainmenu', 'Main menu:')"),
-
-    bdoc("Lock screen"),
-    kpress(META.."F7", "notioncore.exec_on(_, SCREENLOCK)"),
-
-    kpress("XF86AudioPlay", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause')"),
-    kpress("XF86AudioNext", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next')"),
-    kpress("XF86AudioPrev", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous')"),
-    kpress("XF86AudioLowerVolume", "notioncore.exec('$HOME/.zsh/robs/bin/volume down')"),
-    kpress("XF86AudioRaiseVolume", "notioncore.exec('$HOME/.zsh/robs/bin/volume up')"),
-    kpress("XF86AudioMute", "notioncore.exec('$HOME/.zsh/robs/bin/volume mute')"),
-})
+--defbindings("WIonWS", {
+--    bdoc("Split current frame vertically."),
+--    kpress(MOD1.."V", "WIonWS.split_at(_, _sub, 'bottom', true)"),
+--
+--    bdoc("Split current frame horizontally."),
+--    kpress(MOD1.."H", "WIonWS.split_at(_, _sub, 'right', true)"),
+--    
+--    bdoc("Destroy current frame."),
+--    kpress(MOD1.."Tab", "WIonWS.unsplit_at(_, _sub)"),
+--
+--    bdoc("Go to frame above/below/right/left of current frame."),
+--    kpress(MOD2.."Up", "WIonWS.goto_dir(_, 'above')"),
+--    kpress(MOD2.."Down", "WIonWS.goto_dir(_, 'below')"),
+--    kpress(MOD2.."Right", "WIonWS.goto_dir(_, 'right')"),
+--    kpress(MOD2.."Left", "WIonWS.goto_dir(_, 'left')"),
+--    submap(MOD1.."K", {
+--        bdoc("Split current frame horizontally."),
+--        kpress("S", "WIonWS.split_at(_, _sub, 'right', true)"),
+--        
+--        bdoc("Destroy current frame."),
+--        kpress("X", "WIonWS.unsplit_at(_, _sub)"),
+--    }),
+--})
+--
+--
+--defbindings("WScreen", {
+--    kpress(META.."t", "notioncore.exec_on(_, XTERM or 'x-terminal-emulator')"),
+--    kpress(META.."Tab", "ioncore.goto_next()"),
+--    --kpress("K", "ioncore.goto_previous()"),
+--    kpress(META.."N", "ioncore.create_ws(_)"),
+--})
+--
+--defbindings("WFrame", {
+--    submap(META.."k", {
+--      kpress(META.."n", "_:switch_next()"),
+--      kpress(META.."p", "_:switch_prev()"),
+--    }),
+----    kpress(META.."t", "notioncore.exec_on(_, XTERM or 'x-terminal-emulator')"),
+----    submap(META.."k", {
+----      kpress(META.."p", "_:switch_prev()"),
+----      kpress(META.."r", "ioncore.restart()"),
+----      kpress(META.."n", "_:switch_next()"),
+----    }),
+--})
+--
+--defbindings("WMPlex.toplevel", {
+--    kpress(META.."c", "WClientWin.kill(_sub)"),
+--    --submap(META.."k", {
+--        --kpress(META.."c", "WClientWin.kill(_sub)", "_sub:WClientWin"),
+--    --})
+--})
+--
+----defbindings("WMPlex.toplevel", {
+----    kpress(META.."F1", nil),
+----    kpress(META.."F2", nil),
+----    kpress(META.."F3", nil),
+----    kpress(META.."F4", nil),
+----    kpress(META.."F5", nil),
+----    kpress(META.."F6", nil),
+----    kpress(META.."F7", nil),
+----    kpress(META.."F8", nil),
+----    kpress(META.."F9", nil),
+----    kpress(META.."F10", nil),
+----    kpress(META.."F11", nil),
+----    kpress(META.."F12", nil),
+----    kpress(META.."L", nil),
+----})
+--
+--defbindings("WScreen", {
+--    kpress(META.."F2", "notioncore.exec_on(_, XTERM or 'x-terminal-emulator')"),
+--    kpress(META.."F3", "mod_query.query_exec(_)"),
+--    kpress(META.."F4", "notioncore.exec_on(_, BROWSER or 'x-browser')"),
+--    kpress(META.."F5", "notioncore.exec_on(_, BROWSER_SECRET or 'x-browser-secret')"),
+--    --kpress(META.."F6", "notioncore.exec_on(_, MUSIC or 'x-music')"),
+--    kpress(META.."F10", "ioncore.create_ws(_)"),
+--    kpress(META.."F11", "ioncore.restart()"),
+--    kpress(META.."F12", "mod_query.query_menu(_, _sub, 'mainmenu', 'Main menu:')"),
+--  --bdoc("Lock screen"),
+--
+--    --kpress(META.."F7", "notioncore.exec_on(_, SCREENLOCK)"),
+--
+--    --kpress("XF86AudioPlay", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause')"),
+--    --kpress("XF86AudioNext", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next')"),
+--    --kpress("XF86AudioPrev", "notioncore.exec('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous')"),
+--    --kpress("XF86AudioLowerVolume", "notioncore.exec('$HOME/.zsh/robs/bin/volume down')"),
+--    --kpress("XF86AudioRaiseVolume", "notioncore.exec('$HOME/.zsh/robs/bin/volume up')"),
+--    --kpress("XF86AudioMute", "notioncore.exec('$HOME/.zsh/robs/bin/volume mute')"),
+--})
 
 if not gr.select_engine("de") then return end
 
@@ -54,7 +106,7 @@ de.defstyle("*", {
 	highlight_pixels  = 0,
 	padding_pixels    = 0,
 	spacing           = 0,
-  font              = "-*-lucidia-*-r-*--20-*-*-*-*-*-*-*",
+  font              = "-*-lucidia-*-r-*--14-*-*-*-*-*-*-*",
 	text_align        = "center",
 })
 
