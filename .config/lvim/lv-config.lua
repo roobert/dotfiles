22c5b37ee8730e5979e465f3cffec9c0a1a63a6c
Rob Wilson <roobert@gmail.com>
2021-07-21 19:49:00 +0100

    updated

--- .config/lvim/lv-config.lua
+++ .config/lvim/lv-config.lua
@@ -132,7 +132,6 @@ O.lang.latex.latexindent.modify_line_breaks = false
 --   },
 -- }
 
-
 -- this apparently does nothing..
 --O.auto_close_tree = 1
 
@@ -164,7 +163,7 @@ O.lang.python.analysis.type_checking = "on"
 O.lang.python.analysis.auto_search_paths = true
 O.lang.python.analysis.use_library_code_types = true
 O.lang.python.formatter.exe = "black"
-O.lang.python.formatter.args = {"-q -"}
+O.lang.python.formatter.args = { "-q -" }
 O.lang.python.formatter.stdin = true
 
 O.lang.sh.linter = "shellcheck"
@@ -174,107 +173,109 @@ O.lang.sh.linter = "shellcheck"
 -- install linters:
 
 O.user_plugins = {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    setup = function()
-      vim.g.indentLine_enabled = 1
-      vim.g.indent_blankline_char = "▏"
-      vim.g.indent_blankline_buftype_exclude = { "terminal" }
-      vim.g.indent_blankline_show_trailing_blankline_indent = false
-      vim.g.indent_blankline_show_first_indent_level = true
-      vim.g.indent_blankline_filetype_exclude = {
-        "help",
-        "terminal",
-        "dashboard",
-      }
-    end
-  }, -- indent blank lines for nice indent guides
-  { "roobert/nightshift.vim" }, -- my new cool theme!
-  { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }, -- improved fuzzy search
-  { "ray-x/lsp_signature.nvim" }, -- signatures for functions
-  { "tpope/vim-commentary" }, -- toggle commenting
-  { "onsails/lspkind-nvim" }, -- text objects for parenthesis, brackets, quotes, etc.
-  { "tpope/vim-surround" }, -- add around objects
-  { "andymass/vim-matchup", event = "VimEnter" }, -- more text objects - allow changing values in next object without being inside it
-  { "wellle/targets.vim" }, -- use '%' to jump between if/end/else, etc.
-  {
-    "folke/todo-comments.nvim",
-    requires = "nvim-lua/plenary.nvim",
-    config = function()
-      require("todo-comments").setup({
-        highlight = {
-          before = "", -- "fg" or "bg" or empty
-          keyword = "bg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-          after = "fg", -- "fg" or "bg" or empty
-          pattern = [[.*<(KEYWORDS)\s*]], -- pattern used for highlightng (vim regex)
-          comments_only = true, -- uses treesitter to match keywords in comments only
-          max_line_len = 400, -- ignore lines longer than this
-          exclude = {}, -- list of file types to exclude highlighting
-        },
-        search = {
-          command = "rg",
-          args = {
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-          },
-          pattern = [[\b(KEYWORDS)\b]],
-        },
-      })
-    end,
-  }, -- Highlight FIXME, TODO, NOTE, etc.
-  {
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-    config = function()
-      require("trouble").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      })
-    end,
-  }, -- improved debug
-  { "roobert/robs.vim" }, -- Nice interface for displaying nvim diagnostics
-  { "junegunn/vim-easy-align" }, -- Visual-block+enter to align stuff
-  { "ludovicchabant/vim-gutentags",
-    setup = function()
-      vim.g.gutentags_modules = {'ctags'}
-      vim.g.gutentags_project_root = {'.git'}
-      vim.g.gutentags_add_default_project_roots = 0
-      vim.g.gutentags_define_advanced_commands = 1
-      vim.g.gutentags_cache_dir = os.getenv('HOME') .. '/.cache/tags'
-    end
-  }, -- Auto handle ctags to allow jumping to definitions
-  --{'majutsushi/tagbar'}, -- Sidebar to show ctags
-  { "simrat39/symbols-outline.nvim" }, -- Sidebar to show symbols
-  { "kosayoda/nvim-lightbulb" }, -- Hint about code actions to make them discoverable
-  { "jeffkreeftmeijer/vim-numbertoggle" }, -- auto switch between relative and normal line numbers
-  { "f-person/git-blame.nvim" }, -- git blame support
-  {
-    "ahmedkhalf/lsp-rooter.nvim",
-    event = "BufRead",
-    config = function()
-      require("lsp-rooter").setup()
-    end,
-  }, -- pwd changes to project
-  { "kevinhwang91/nvim-bqf" }, -- better quick fix
-  { "can3p/incbool.vim" }, -- toggle booleans with c-x
-  {'karb94/neoscroll.nvim'}, -- smooth scrolling
-  {'chaoren/vim-wordmotion'}, -- set useful word boundaries for camel case and snake case
-  {'rktjmp/lush.nvim'}, -- colorscheme creator
-  {'nvim-treesitter/playground'}, -- Add playground until it's re-enabled in core
-  { 'ibhagwan/fzf-lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional for icons
-      'vijaymarupudi/nvim-fzf' }
-  }, -- fzf instead of telescope
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		setup = function()
+			vim.g.indentLine_enabled = 1
+			vim.g.indent_blankline_char = "▏"
+			vim.g.indent_blankline_buftype_exclude = { "terminal" }
+			vim.g.indent_blankline_show_trailing_blankline_indent = false
+			vim.g.indent_blankline_show_first_indent_level = true
+			vim.g.indent_blankline_filetype_exclude = {
+				"help",
+				"terminal",
+				"dashboard",
+			}
+		end,
+	}, -- indent blank lines for nice indent guides
+	{ "roobert/nightshift.vim" }, -- my new cool theme!
+	{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }, -- improved fuzzy search
+	{ "ray-x/lsp_signature.nvim" }, -- signatures for functions
+	{ "tpope/vim-commentary" }, -- toggle commenting
+	{ "onsails/lspkind-nvim" }, -- text objects for parenthesis, brackets, quotes, etc.
+	{ "tpope/vim-surround" }, -- add around objects
+	{ "andymass/vim-matchup", event = "VimEnter" }, -- more text objects - allow changing values in next object without being inside it
+	{ "wellle/targets.vim" }, -- use '%' to jump between if/end/else, etc.
+	{
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup({
+				highlight = {
+					before = "", -- "fg" or "bg" or empty
+					keyword = "bg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+					after = "fg", -- "fg" or "bg" or empty
+					pattern = [[.*<(KEYWORDS)\s*]], -- pattern used for highlightng (vim regex)
+					comments_only = true, -- uses treesitter to match keywords in comments only
+					max_line_len = 400, -- ignore lines longer than this
+					exclude = {}, -- list of file types to exclude highlighting
+				},
+				search = {
+					command = "rg",
+					args = {
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+					},
+					pattern = [[\b(KEYWORDS)\b]],
+				},
+			})
+		end,
+	}, -- Highlight FIXME, TODO, NOTE, etc.
+	{
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	}, -- improved debug
+	{ "roobert/robs.vim" }, -- Nice interface for displaying nvim diagnostics
+	{ "junegunn/vim-easy-align" }, -- Visual-block+enter to align stuff
+	{
+		"ludovicchabant/vim-gutentags",
+		setup = function()
+			vim.g.gutentags_modules = { "ctags" }
+			vim.g.gutentags_project_root = { ".git" }
+			vim.g.gutentags_add_default_project_roots = 0
+			vim.g.gutentags_define_advanced_commands = 1
+			vim.g.gutentags_cache_dir = os.getenv("HOME") .. "/.cache/tags"
+		end,
+	}, -- Auto handle ctags to allow jumping to definitions
+	--{'majutsushi/tagbar'}, -- Sidebar to show ctags
+	{ "simrat39/symbols-outline.nvim" }, -- Sidebar to show symbols
+	{ "kosayoda/nvim-lightbulb" }, -- Hint about code actions to make them discoverable
+	{ "jeffkreeftmeijer/vim-numbertoggle" }, -- auto switch between relative and normal line numbers
+	{ "f-person/git-blame.nvim" }, -- git blame support
+	{
+		"ahmedkhalf/lsp-rooter.nvim",
+		event = "BufRead",
+		config = function()
+			require("lsp-rooter").setup()
+		end,
+	}, -- pwd changes to project
+	{ "kevinhwang91/nvim-bqf" }, -- better quick fix
+	{ "can3p/incbool.vim" }, -- toggle booleans with c-x
+	{ "karb94/neoscroll.nvim" }, -- smooth scrolling
+	{ "chaoren/vim-wordmotion" }, -- set useful word boundaries for camel case and snake case
+	{ "rktjmp/lush.nvim" }, -- colorscheme creator
+	{ "nvim-treesitter/playground" }, -- Add playground until it's re-enabled in core
+	{
+		"ibhagwan/fzf-lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional for icons
+			"vijaymarupudi/nvim-fzf",
+		},
+	}, -- fzf instead of telescope
 }
 
-
 O.user_which_key = {
-  x = { "<cmd>TroubleToggle lsp_document_diagnostics<cr>", "Display Trouble document diagnostics" },
+	x = { "<cmd>TroubleToggle lsp_document_diagnostics<cr>", "Display Trouble document diagnostics" },
 }
 
 -- FIXME
@@ -296,28 +297,28 @@ O.user_which_key = {
 --
 --require("telescope").load_extension("fzf")
 
-require('neoscroll').setup()
+require("neoscroll").setup()
 
-require "nvim-treesitter.configs".setup {
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = 'o',
-      toggle_hl_groups = 'i',
-      toggle_injected_languages = 't',
-      toggle_anonymous_nodes = 'a',
-      toggle_language_display = 'I',
-      focus_language = 'f',
-      unfocus_language = 'F',
-      update = 'R',
-      goto_node = '<cr>',
-      show_help = '?',
-    },
-  }
-}
+require("nvim-treesitter.configs").setup({
+	playground = {
+		enable = true,
+		disable = {},
+		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		persist_queries = false, -- Whether the query persists across vim sessions
+		keybindings = {
+			toggle_query_editor = "o",
+			toggle_hl_groups = "i",
+			toggle_injected_languages = "t",
+			toggle_anonymous_nodes = "a",
+			toggle_language_display = "I",
+			focus_language = "f",
+			unfocus_language = "F",
+			update = "R",
+			goto_node = "<cr>",
+			show_help = "?",
+		},
+	},
+})
 
 --vim.api.nvim_set_keymap("n", "<leader>F", '<CMD>silent Format<CR>', { noremap = true, silent = true })
 --vim.cmd [[nnoremap <c-f> <cmd>lua require('fzf-lua').files()<CR>]]
@@ -367,36 +368,35 @@ require "nvim-treesitter.configs".setup {
 --vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
 
 -- automatically switch between relative and absolute line numbers
-vim.cmd [[set number relativenumber]]
+vim.cmd([[set number relativenumber]])
 
 -- FIXME: none of these work
 function map(mode, lhs, rhs, opts)
-    local options = {noremap = true}
-    if opts then
-        options = vim.tbl_extend('force', options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- Search for word under cursor
-map('n', '<leader>*', '*N', {noremap = true, silent = true})
+map("n", "<leader>*", "*N", { noremap = true, silent = true })
 
 -- s/// shortcut
-map('n', '<leader>S', ':%s//gcI<Left><Left><Left><Left>', {silent = false})
-map('v', '<leader>S', ':s//gcI<Left><Left><Left><Left>', {silent = false})
+map("n", "<leader>S", ":%s//gcI<Left><Left><Left><Left>", { silent = false })
+map("v", "<leader>S", ":s//gcI<Left><Left><Left><Left>", { silent = false })
 
 -- Toggle CTags sidebar..
-map('n', '<leader>C', '<cmd>TagbarToggle<CR>')
+map("n", "<leader>C", "<cmd>TagbarToggle<CR>")
 
 -- Execute code action
-map('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map("n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>")
 
 -- Easily escape out of Terminal mode
-map('t', '<leader><escape>', '<C-\\><C-n>')
+map("t", "<leader><escape>", "<C-\\><C-n>")
 
 -- Paste last yank, nb: pressing '"' allows which-key to show register yank history
-map('n', '<leader>P', '"0p')
-
+map("n", "<leader>P", '"0p')
 
 -- FIXME: these are ignored
 
@@ -411,8 +411,8 @@ map('n', '<leader>P', '"0p')
 --}
 
 -- Undo setting from lunarvim - wrapping cursor movement across lines
-vim.cmd "set whichwrap=b,s"
-vim.cmd "set iskeyword+=_"
+vim.cmd("set whichwrap=b,s")
+vim.cmd("set iskeyword+=_")
 
 -- NOTE
 -- * See :PackerStatus for loaded plugins..
@@ -507,75 +507,78 @@ vim.cmd "set iskeyword+=_"
 --    use {'9mm/vim-closer'}
 -- end)
 
-require'lsp_signature'.on_attach({
-    bind = true, -- This is mandatory, otherwise border config won't get registered.
-    -- If you want to hook lspsaga or other signature handler, pls set to false
-    doc_lines = 2, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
-    -- set to 0 if you DO NOT want any API comments be shown
-    -- This setting only take effect in insert mode, it does not affect signature help in normal
-    -- mode, 10 by default
+require("lsp_signature").on_attach({
+	bind = true, -- This is mandatory, otherwise border config won't get registered.
+	-- If you want to hook lspsaga or other signature handler, pls set to false
+	doc_lines = 2, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+	-- set to 0 if you DO NOT want any API comments be shown
+	-- This setting only take effect in insert mode, it does not affect signature help in normal
+	-- mode, 10 by default
 
-    floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
-    hint_enable = true, -- virtual hint enable
-    hint_prefix = " 👉 ", -- Panda for parameter
-    fix_pos = true,
-    hint_scheme = "String",
-    use_lspsaga = false, -- set to true if you want to use lspsaga popup
-    hi_parameter = "Search", -- how your parameter will be highlight
-    max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
-    -- to view the hiding contents
-    max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
-    handler_opts = {
-        border = "single" -- double, single, shadow, none
-    },
-    extra_trigger_chars = {} -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
-    -- deprecate !!
-    -- decorator = {"`", "`"}  -- this is no longer needed as nvim give me a handler and it allow me to highlight active parameter in floating_window
+	floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
+	hint_enable = true, -- virtual hint enable
+	hint_prefix = " 👉 ", -- Panda for parameter
+	fix_pos = true,
+	hint_scheme = "String",
+	use_lspsaga = false, -- set to true if you want to use lspsaga popup
+	hi_parameter = "Search", -- how your parameter will be highlight
+	max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
+	-- to view the hiding contents
+	max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+	handler_opts = {
+		border = "single", -- double, single, shadow, none
+	},
+	extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+	-- deprecate !!
+	-- decorator = {"`", "`"}  -- this is no longer needed as nvim give me a handler and it allow me to highlight active parameter in floating_window
 })
 
 -- icons for signatures
-require('lspkind').init({
-    -- enables text annotations
-    --
-    -- default: true
-    with_text = true,
+require("lspkind").init({
+	-- enables text annotations
+	--
+	-- default: true
+	with_text = true,
 
-    -- default symbol map
-    -- can be either 'default' or
-    -- 'codicons' for codicon preset (requires vscode-codicons font installed)
-    --
-    -- default: 'default'
-    preset = 'codicons',
+	-- default symbol map
+	-- can be either 'default' or
+	-- 'codicons' for codicon preset (requires vscode-codicons font installed)
+	--
+	-- default: 'default'
+	preset = "codicons",
 
-    -- override preset symbols
-    --
-    -- default: {}
-    symbol_map = {
-        Text = '',
-        Method = 'ƒ',
-        Function = '',
-        Constructor = '',
-        Variable = '',
-        Class = '',
-        Interface = 'ﰮ',
-        Module = '',
-        Property = '',
-        Unit = '',
-        Value = '',
-        Enum = '了',
-        Keyword = '',
-        Snippet = '﬌',
-        Color = '',
-        File = '',
-        Folder = '',
-        EnumMember = '',
-        Constant = '',
-        Struct = ''
-    }
+	-- override preset symbols
+	--
+	-- default: {}
+	symbol_map = {
+		Text = "",
+		Method = "ƒ",
+		Function = "",
+		Constructor = "",
+		Variable = "",
+		Class = "",
+		Interface = "ﰮ",
+		Module = "",
+		Property = "",
+		Unit = "",
+		Value = "",
+		Enum = "了",
+		Keyword = "",
+		Snippet = "﬌",
+		Color = "",
+		File = "",
+		Folder = "",
+		EnumMember = "",
+		Constant = "",
+		Struct = "",
+	},
 })
 
-vim.cmd [[autocmd FileType text,latex,tex,md,markdown setlocal spell]]
+vim.cmd([[autocmd FileType text,latex,tex,md,markdown setlocal spell]])
 
-vim.api.nvim_set_keymap("n", "<leader>dD", "<cmd>Trouble lsp_document_diagnostics<cr>",
-  {silent = true, noremap = true}
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>dD",
+	"<cmd>Trouble lsp_document_diagnostics<cr>",
+	{ silent = true, noremap = true }
 )
