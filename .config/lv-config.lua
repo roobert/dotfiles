c2d5a90f76f2179de556962585b0799a78657bb9
Rob Wilson <roobert@gmail.com>
2021-08-21 18:58:48 -0500

    Updated

--- .config/lv-config.lua
+++ .config/lv-config.lua
@@ -368,6 +368,11 @@ vim.cmd("set iskeyword+=_")
 
 vim.cmd("autocmd FileType text,latex,tex,md,markdown setlocal spell")
 
+vim.cmd([[autocmd BufNewFile *.sh 0put = \"#!/usr/bin/env bash\nset -euo pipefail\" | normal G]])
+vim.cmd(
+	[[autocmd BufNewFile *.py 0put = \"#!/usr/bin/env python\ndef main():\n  pass\nif __name__ == '__main__':\n  main()\n\" | normal G]]
+)
+
 vim.opt.colorcolumn = "80"
 vim.opt.textwidth = 80
 vim.opt.formatoptions = "tcorqnjv"
