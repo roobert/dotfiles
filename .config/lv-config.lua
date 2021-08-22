0d0a91e9446d9545defcb55b09a82b4ba3e0a9bd
Rob Wilson <roobert@gmail.com>
2021-08-21 19:04:06 -0500

    Updated

--- .config/lv-config.lua
+++ .config/lv-config.lua
@@ -457,3 +457,10 @@ vim.opt.formatoptions = "tcorqnjv"
 --  precedes = "⟨",
 --  space = "␣",
 --}
+
+-- vim easy align
+-- start interactive EasyAlign in visual mode (e.g. vip<Enter>)
+--vmap <Enter> <Plug>(EasyAlign)
+--
+-- Start interactive EasyAlign for a motion/text object (e.g. <Leader>aip)
+-- nmap <Leader>a <Plug>(EasyAlign)
