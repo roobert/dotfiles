ea1285f856aeff53c3d981da9553c5f9e8b0d809
Rob Wilson <roobert@gmail.com>
2021-11-25 21:20:49 +0000

    updated

--- .zsh/robs/lscolours.zsh
+++ .zsh/robs/lscolours.zsh
@@ -4,9 +4,9 @@
 if type dircolors 2>&1 > /dev/null; then
   eval $(dircolors -b $HOME/.zsh/lscoloursrc)
 else
-  if [[ -x /usr/local/opt/coreutils/libexec/gnubin/dircolors ]]; then
+  if [[ -x /opt/homebrew//Cellar/coreutils/9.0/libexec/gnubin/dircolors ]]; then
     if ! echo $PATH | grep "coreutils" 2> /dev/null; then
-      export PATH="/usr/local/opt/coreutils/libexec/gnubin/:$PATH"
+      export PATH="/opt/homebrew/Cellar/coreutils/9.0/libexec/gnubin:$PATH"
     fi
 
     eval $(dircolors -b $HOME/.zsh/lscoloursrc)
