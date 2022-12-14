2c4703c5921f9272ff24e62ee8cf4fd5117db917
Rob Wilson <roobert@gmail.com>
2022-12-14 14:23:57 +0000

    Updated

---
+++ .config/lvim/lua/user/treesitter.lua
@@ -0,0 +1,20 @@
+lvim.builtin.treesitter.ensure_installed = {
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"tsx",
+	"css",
+	"rust",
+	"java",
+	"yaml",
+	"go",
+	"hcl",
+	"markdown",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
