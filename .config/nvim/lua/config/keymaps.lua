162e0aba8a5f9338213910d9863a7343f6d01851
Rob Wilson <roobert@gmail.com>
2023-07-25 00:09:16 +0100

    Updated

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local opt = vim.opt
+
+opt.wrap = true
+opt.textwidth = 88
+opt.linebreak = true
+opt.inccommand = "split"
+
+vim.g.markdown_recommended_style = 0
