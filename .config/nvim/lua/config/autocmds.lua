95375c722b3a4ceb090cc0de38ac9949bb790885
Rob Wilson <roobert@gmail.com>
2023-07-25 00:09:16 +0100

    Updated

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.cmd([[autocmd FileType text,latex,tex,md,markdown setlocal spell]])
+
+vim.cmd([[autocmd BufNewFile *.sh 0put = \"#!/usr/bin/env bash\nset -euo pipefail\" | normal G]])
+
+vim.cmd(
+  [[autocmd BufNewFile *.py 0put = \"#!/usr/bin/env python\n\n\ndef main():\n    pass\n\n\nif __name__ == '__main__':\n    main()\" | normal G]]
+)
--- .config/nvim/lua/plugins/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- vim.cmd([[autocmd FileType text,latex,tex,md,markdown setlocal spell]])
---
--- vim.cmd([[autocmd BufNewFile *.sh 0put = \"#!/usr/bin/env bash\nset -euo pipefail\" | normal G]])
---
--- vim.cmd(
---   [[autocmd BufNewFile *.py 0put = \"#!/usr/bin/env python\n\n\ndef main():\n    pass\n\n\nif __name__ == '__main__':\n    main()\" | normal G]]
--- )
-
-return {}
