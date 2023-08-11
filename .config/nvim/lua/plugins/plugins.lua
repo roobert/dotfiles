c16e49895f5068e5d20a1dccbca041c121857365
Rob Wilson <roobert@gmail.com>
2023-08-11 22:30:28 +0100

    Updated

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -316,8 +316,19 @@ return {
   -- tab navigation jumping between significant code elements, such as brackets, quotes, etc.
   {
     "roobert/tabtree.nvim",
+    -- dir = "/Users/rw/git/tabtree.nvim",
+    -- name = "tabtree",
     config = function()
       require("tabtree").setup()
+    end,
+  },
+
+  {
+    "roobert/hoversplit.nvim",
+    -- dir = "/Users/rw/git/hoversplit.nvim",
+    -- name = "hoversplit",
+    config = function()
+      require("hoversplit").setup()
     end,
   },
 
@@ -353,38 +364,6 @@ return {
       })
       require("telescope").load_extension("fzf")
     end,
-  },
-
-  -- FIXME: this would be nice.. probably needs customizing cmp
-  {
-    "abecodes/tabout.nvim",
-    config = function()
-      require("tabout").setup({
-        tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
-        backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
-        act_as_tab = true, -- shift content if tab out is not possible
-        act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-        default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-        default_shift_tab = "<C-d>", -- reverse shift default action,
-        enable_backwards = true, -- well ...
-        completion = true, -- if the tabkey is used in a completion pum
-        tabouts = {
-          { open = "'", close = "'" },
-          { open = '"', close = '"' },
-          { open = "`", close = "`" },
-          { open = "(", close = ")" },
-          { open = "[", close = "]" },
-          { open = "{", close = "}" },
-        },
-        ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-        exclude = {}, -- tabout will ignore these filetypes
-      })
-    end,
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "L3MON4D3/LuaSnip",
-      "hrsh7th/nvim-cmp",
-    },
   },
 
   -- prefer fzf to telescope for fuzzy finding stuff
