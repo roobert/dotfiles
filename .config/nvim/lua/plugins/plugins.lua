4ad0420693935f3f71c06ce667708a63bebdc4bd
Rob Wilson <roobert@gmail.com>
2023-08-12 02:05:22 +0100

    Updated

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -134,11 +134,11 @@ return {
 
   {
     -- "roobert/statusline-action-hints.nvim",
-    dir = "/Users/rw/git/statusline-action-hints.nvim",
-    name = "statusline-action-hints",
+    dir = "/Users/rw/git/action-hints.nvim",
+    name = "action-hints",
     dependencies = "roobert/nightshift.vim",
     config = function()
-      require("statusline-action-hints").setup({
+      require("action-hints").setup({
         use_virtual_text = true,
       })
     end,
@@ -823,6 +823,74 @@ return {
   },
 
   {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+    },
+    opts = {
+      options = {
+        separator_style = "slant",
+        close_command = function(n)
+          require("mini.bufremove").delete(n, false)
+        end,
+        right_mouse_command = function(n)
+          require("mini.bufremove").delete(n, false)
+        end,
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = require("lazyvim.config").icons.diagnostics
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+      highlights = {
+        fill = {
+          bg = "#252d52",
+        },
+
+        separator_selected = {
+          fg = "#252d52",
+        },
+
+        separator_visible = {
+          fg = "#252d52",
+        },
+
+        separator = {
+          fg = "#252d52",
+        },
+
+        buffer_visible = {
+          fg = "#9696ca",
+          bold = false,
+        },
+
+        buffer_selected = {
+          --fg = "#bae1ff",
+          fg = "#eeeeee",
+          bold = false,
+        },
+
+        tab_selected = {
+          bold = false,
+        },
+      },
+    },
+  },
+
+  {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function()
@@ -867,7 +935,7 @@ return {
             --   require("node-type").statusline,
             -- },
             {
-              require("statusline-action-hints").statusline,
+              require("action-hints").statusline,
             },
             {
               function()
