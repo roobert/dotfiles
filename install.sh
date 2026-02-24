f4214e5961e54c16c36c5dfb6e8033ae7153c500
Rob Wilson <roobert@gmail.com>
2026-02-24 06:44:21 -0800

    updated

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ set -e
 DOTFILES_DIR="$HOME/.dotfiles"
 DOTFILES_GIT="/usr/bin/git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
 
+echo "installing dotfiles..."
 $DOTFILES_GIT config --local status.showUntrackedFiles no
 $DOTFILES_GIT checkout -f
+echo "done"
