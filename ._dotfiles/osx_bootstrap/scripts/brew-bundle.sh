a157d4d0c512114b4fb5120cef49537f2b4a11da
Rob Wilson <roobert@gmail.com>
2026-03-04 22:59:50 +0000

    updated

--- ._dotfiles/osx_bootstrap/scripts/brew-bundle.sh
+++ ._dotfiles/osx_bootstrap/scripts/brew-bundle.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # Install packages from Brewfile
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BREWFILE="${SCRIPT_DIR}/../Brewfile"
+_brew_bundle_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BREWFILE="${_brew_bundle_dir}/../Brewfile"
+unset _brew_bundle_dir
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew bundle --file="$BREWFILE"
